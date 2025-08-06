56fc8a9ab7ade196cf02b66e42ec80b4475829f5
Richard Zhou <richardzhou163@gmail.com>
2025-08-06 19:02:47 -0400

    update: added more context in api response class

--- src/main/java/com/richardz02/personal_finance/dto/ApiResponse.java
+++ src/main/java/com/richardz02/personal_finance/dto/ApiResponse.java
@@ -1,10 +1,31 @@
 package com.richardz02.personal_finance.dto;
 
 public class ApiResponse {
+    public enum Status {
+        SUCCESS(true),
+        FAILURE(false);
+
+        private final boolean isSuccess;
+
+        Status(boolean isSuccess) {
+            this.isSuccess = isSuccess;
+        }
+
+        public boolean isSuccess() {
+            return isSuccess;
+        }
+    }
+
+    private Status status;
     private String message;
 
-    public ApiResponse(String message) {
+    public ApiResponse(Status status,String message) {
+        this.status= status;
         this.message = message;
+    }
+
+    public boolean getSuccess() {
+        return status.isSuccess();
     }
 
     public String getMessage() {
