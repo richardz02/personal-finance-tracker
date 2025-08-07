6b4b7967bb6b8d8e0576b055a8ecf5385fe87e9e
Richard Zhou <richardzhou163@gmail.com>
2025-08-07 11:14:28 -0400

    Disabled Spring Security default login page

--- src/main/java/com/richardz02/personal_finance/PersonalFinanceApplication.java
+++ src/main/java/com/richardz02/personal_finance/PersonalFinanceApplication.java
@@ -2,8 +2,9 @@ package com.richardz02.personal_finance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class PersonalFinanceApplication {
 
 	public static void main(String[] args) {
