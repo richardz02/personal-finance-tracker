8f778c5781d67b6adf48f28daa54ae2e5f100835
Richard Zhou <richardzhou163@gmail.com>
2025-08-11 17:49:22 -0400

    update: change id type from string to uuid

--- src/main/java/com/richardz02/personal_finance/exception/transaction/TransactionNotFoundException.java
+++ src/main/java/com/richardz02/personal_finance/exception/transaction/TransactionNotFoundException.java
@@ -1,7 +1,9 @@
 package com.richardz02.personal_finance.exception.transaction;
 
+import java.util.UUID;
+
 public class TransactionNotFoundException extends RuntimeException {
-    public TransactionNotFoundException(String id) {
+    public TransactionNotFoundException(UUID id) {
         super("Transaction with id <" + id + "> not found");
     } 
 }
