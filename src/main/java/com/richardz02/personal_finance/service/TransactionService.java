842056bab26343260b058d0cbc58583e6ab12985
Richard Zhou <richardzhou163@gmail.com>
2025-09-28 14:47:02 -0400

    forgot to commit this 2 commits ago, oops

--- src/main/java/com/richardz02/personal_finance/service/TransactionService.java
+++ src/main/java/com/richardz02/personal_finance/service/TransactionService.java
@@ -5,8 +5,9 @@ import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
-import com.richardz02.personal_finance.dto.transaction.TransactionRequestDTO;
-import com.richardz02.personal_finance.dto.transaction.TransactionResponseDTO;
+import com.richardz02.personal_finance.dto.transaction.TransactionRequest;
+import com.richardz02.personal_finance.dto.transaction.TransactionResponse;
+import com.richardz02.personal_finance.dto.transaction.TransactionSummary;
 import com.richardz02.personal_finance.exception.transaction.NoTransactionsException;
 import com.richardz02.personal_finance.exception.transaction.TransactionNotFoundException;
 import com.richardz02.personal_finance.model.Transaction;
@@ -26,20 +27,43 @@ public class TransactionService {
         return transactionRepository.findAll();        
     }
 
-    public void addTransaction(TransactionRequestDTO transactionRequest) {
+    public Transaction addTransaction(TransactionRequest transactionRequest) {
+        // Create a new transaction object
         Transaction transaction = new Transaction();
+
+        // Set the fields in the object
         transaction.setTransactionType(transactionRequest.getTransactionType());
         transaction.setAmount(transactionRequest.getAmount());
         transaction.setDescription(transactionRequest.getDescription());
+
+        // Save the transaction object to the database
         transactionRepository.save(transaction);
+
+        return transaction;
     }
 
-    public TransactionResponseDTO getTransactionById(UUID id) {
+    public Transaction updateTransaction(UUID id, TransactionRequest transactionRequest) {
+        // Get the transaction object from the database by id
+        Transaction transaction = transactionRepository.findById(id).orElseThrow(() -> new TransactionNotFoundException(id));
+
+        // Reset the fields of this transaction object
+        transaction.setTransactionType(transactionRequest.getTransactionType());
+        transaction.setAmount(transactionRequest.getAmount());
+        transaction.setDescription(transactionRequest.getDescription());
+
+        // Save the updated transaction to database
+        transactionRepository.save(transaction);
+
+        // Return the updated transaction
+        return transaction;
+    }
+
+    public TransactionResponse getTransactionById(UUID id) {
         // Get the Transaction object from the database
         Transaction transaction = transactionRepository.findById(id).orElseThrow(() -> new TransactionNotFoundException(id));
 
         // Construct a new instance of TransactionResponseDTO 
-        TransactionResponseDTO transactionResponse = new TransactionResponseDTO(
+        TransactionResponse transactionResponse = new TransactionResponse(
             transaction.getDate(),
             TransactionType.valueOf(transaction.getTransactionType()),
             transaction.getAmount(),
@@ -58,5 +82,9 @@ public class TransactionService {
         // Check if the transaction can be found in the database
         transactionRepository.findById(id).orElseThrow(() -> new TransactionNotFoundException(id));
         transactionRepository.deleteById(id);
+    }
+
+    public TransactionSummary getSummary() {
+        return transactionRepository.transactionSummary();
     }
 }
