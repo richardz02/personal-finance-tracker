154d573f7c4e1861ddf2f3b0f9678f66f38aee10
Richard Zhou <richardzhou163@gmail.com>
2025-09-28 14:50:46 -0400

    update: added custom method in repostitory to allow more complex queries

--- src/main/java/com/richardz02/personal_finance/repository/TransactionRepository.java
+++ src/main/java/com/richardz02/personal_finance/repository/TransactionRepository.java
@@ -3,11 +3,25 @@ package com.richardz02.personal_finance.repository;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.richardz02.personal_finance.model.Transaction;
+import com.richardz02.personal_finance.dto.transaction.TransactionSummary;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
-    
+    /*  Since we are using enum class to represent the transaction types, in the database
+        income will have a value of 0, and expense will have a value of 1
+    */
+    @Query("""
+            SELECT new com.richardz02.personal_finance.dto.transaction.TransactionSummary (
+                SUM(CASE WHEN t.transactionType = 0 THEN t.amount ELSE 0 END),
+                SUM(CASE WHEN t.transactionType = 1 THEN t.amount ELSE 0 END),
+                SUM(CASE WHEN t.transactionType = 0 THEN t.amount ELSE 0 END) -
+                SUM(CASE WHEN t.transactionType = 1 THEN t.amount ELSE 0 END)
+            )
+            FROM Transaction t
+            """)
+    TransactionSummary transactionSummary();
 }
