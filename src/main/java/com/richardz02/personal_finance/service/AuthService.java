8e208161f4279491e25f43756329f22b33acf1ca
Richard Zhou <richardzhou163@gmail.com>
2025-11-03 16:43:09 -0500

    refactor: let AuthenticationManager from Spring Security to handle user login authentication

--- src/main/java/com/richardz02/personal_finance/service/AuthService.java
+++ src/main/java/com/richardz02/personal_finance/service/AuthService.java
@@ -1,14 +1,18 @@
 package com.richardz02.personal_finance.service;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Service;
 
+import com.richardz02.personal_finance.dto.ApiResponse;
 import com.richardz02.personal_finance.dto.user.UserAuthRequestDTO;
 import com.richardz02.personal_finance.dto.user.UserAuthResponseDTO;
-import com.richardz02.personal_finance.exception.auth.AuthenticationException;
 import com.richardz02.personal_finance.exception.user.UserAlreadyExistsException;
-import com.richardz02.personal_finance.exception.user.UserNotFoundException;
 import com.richardz02.personal_finance.repository.UserRepository;
+import com.richardz02.personal_finance.security.CustomUserDetails;
 import com.richardz02.personal_finance.utility.PasswordUtil;
 import com.richardz02.personal_finance.model.User;
 
@@ -16,34 +20,16 @@ import com.richardz02.personal_finance.model.User;
 public class AuthService {
     private final UserRepository userRepository;
     private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+    private final PasswordEncoder passwordEncoder;
     
-    public AuthService(UserRepository userRepository, JwtService jwtService) {
+    public AuthService(UserRepository userRepository, JwtService jwtService, AuthenticationManager authenticationManager, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.jwtService = jwtService;
+        this.authenticationManager = authenticationManager;
+        this.passwordEncoder = passwordEncoder;
     }
-
-    public ResponseEntity<UserAuthResponseDTO> authenticateUserLogin(UserAuthRequestDTO userLogin) {
-        // Find user by username
-        // if user doesn't exist, throw exception
-        User user = userRepository.findByUsername(userLogin.getUsername()).orElseThrow(() -> new UserNotFoundException("Username not found."));
-
-        // If user exists, compare hashed passwords
-        // If passwords match, user has successfully identified themselves
-        // Send response back along with jwt
-        if (PasswordUtil.verifyPassword(userLogin.getPassword(), user.getpasswordHash())) {
-            String userId = user.getUserId();
-            String username = user.getUserName();
-            String token =  jwtService.generateToken(userId, username);
-
-            UserAuthResponseDTO userAuthResponse = new UserAuthResponseDTO(userId, username);
-            
-            return ResponseEntity.ok().header("Authorization", "Bearer " + token).body(userAuthResponse);
-        }
-        else {
-            throw new AuthenticationException("Incorrect password");
-        }
-    }
-
+    
     public UserAuthResponseDTO userSignupRequest(UserAuthRequestDTO userSignup) {
         if (userRepository.existsByUsername(userSignup.getUsername())) {
             throw new UserAlreadyExistsException("Username is not available");
@@ -54,13 +40,34 @@ public class AuthService {
 
         // Set username and password hash
         user.setUserName(userSignup.getUsername());
-        user.setpasswordHash(PasswordUtil.hashingPassword(userSignup.getPassword()));
+        user.setpasswordHash(passwordEncoder.encode(userSignup.getPassword()));
         
         // Save created user in database
         userRepository.save(user);
 
         UserAuthResponseDTO userSignupResponse = new UserAuthResponseDTO(user.getUserId(), user.getUserName());
-
+        
         return userSignupResponse;
+    }
+
+    public ResponseEntity<ApiResponse<UserAuthResponseDTO>> authenticateUserLogin(UserAuthRequestDTO userLogin) {
+        // Authenticate user login request
+        Authentication authentication = authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(
+                userLogin.getUsername(),
+                userLogin.getPassword()
+        ));
+
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        String userid = userDetails.getUserid();
+        String username = userDetails.getUsername();
+        String token = jwtService.generateToken(userid, username);
+
+        UserAuthResponseDTO userLoginResponse = new UserAuthResponseDTO(userid, username);
+
+        ApiResponse<UserAuthResponseDTO> response = new ApiResponse<>("success", "Login success", userLoginResponse);
+
+        return ResponseEntity.ok().header("Authorization", "Bearer " + token).body(response);
+
     }
 }
