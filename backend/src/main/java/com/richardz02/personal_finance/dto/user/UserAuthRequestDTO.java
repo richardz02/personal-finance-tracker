a49b717468a7dedd2822eda0da6331a50779e449
Richard Zhou <richardzhou163@gmail.com>
2025-11-04 17:14:52 -0500

    add: not null checks for user authentication request

--- backend/src/main/java/com/richardz02/personal_finance/dto/user/UserAuthRequestDTO.java
+++ backend/src/main/java/com/richardz02/personal_finance/dto/user/UserAuthRequestDTO.java
@@ -1,7 +1,12 @@
 package com.richardz02.personal_finance.dto.user;
 
+import jakarta.validation.constraints.NotNull;
+
 public class UserAuthRequestDTO {
+    @NotNull(message = "Username cannot be null")
     private String username;
+
+    @NotNull(message = "Password cannot be null")
     private String password;
     
     public String getUsername() {
